af0e81f8d46595ae9f5659e5b365ea2f7d635132
Péter Erdősi <peter.erdosi@extremalelement.com>
2021-09-27 21:38:45 +0200

    init dotenv and sequileze connection

--- app.js
+++ app.js
@@ -19,14 +19,14 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use('/', indexRouter);
-// app.use('/users', usersRouter);
-app.use(express.static(__dirname + '/dist/dreamjobs4u-fe'));
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+// app.use(express.static(__dirname + '/dist/dreamjobs4u-fe'));
 
-app.get('/*', function(req,res) {
+// app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/dreamjobs4u-fe/index.html'));
-});
+// res.sendFile(path.join(__dirname+'/dist/dreamjobs4u-fe/index.html'));
+// });
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ db/config/sequileze_config.js
@@ -0,0 +1,56 @@
+const dotenv = require("dotenv");
+dotenv.config();
+
+module.exports = {
+  development: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: process.env.DB_DIALECT,
+    dialectOptions: {
+      bigNumberStrings: true
+    },
+    minifyAliases: true,
+    pool: {
+      max: 300,
+      min: 50,
+      idle: 10000
+    }
+  },
+  test: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: process.env.DB_DIALECT,
+    dialectOptions: {
+      bigNumberStrings: true
+    },
+    minifyAliases: true,
+    pool: {
+      max: 300,
+      min: 50,
+      idle: 10000
+    }
+  },
+  production: {
+    username: process.env.DB_DEPLOY_USER,
+    password: process.env.DB_DEPLOY_PASS,
+    database: process.env.DB_DEPLOY_DATABASE,
+    host: process.env.DB_DEPLOY_HOST,
+    port: process.env.DB_DEPLOY_PORT,
+    dialect: process.env.DB_DEPLOY_DIALECT,
+    dialectOptions: {
+      bigNumberStrings: true
+    },
+    minifyAliases: true,
+    pool: {
+      max: 300,
+      min: 50,
+      idle: 10000
+    }
+  }
+};

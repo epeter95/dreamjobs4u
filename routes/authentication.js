aad2ae6ddca28f9cb017f2a569f28568f067215e
Péter Erdősi <peter.erdosi@extremalelement.com>
2021-10-22 16:00:25 +0200

    public login method is implemented

--- routes/authentication.js
+++ routes/authentication.js
@@ -46,4 +46,28 @@ router.post('/login', async (req, res) => {
     }
 });
 
+router.post('/login/public', async (req, res) => {
+    try {
+        const { loginEmail, loginPassword } = req.body;
+        const data = await User.findOne({ where: {email: loginEmail} });
+        if(data){
+            console.log("BEJÖTT")
+            const isAuthenticated = await bcrypt.compare(loginPassword, data.password)
+            if(!isAuthenticated){
+                return res.sendStatus(401);
+            }
+            const privateKey = fs.readFileSync(process.env.JWT_SECRET_KEY, 'utf8');
+            const token = jwt.sign({ email: data.email, role: data.role }, privateKey, {algorithm: 'RS256'});
+            return res.send({token: token});
+        }else{
+            res.send({error: 'userNotExist'})
+        }
+        return res.send({ ok: 'siker' });
+    } catch (error) {
+        console.log(error);
+        console.log(error);
+        return res.send({ error: error.name });
+    }
+});
+
 module.exports = router;

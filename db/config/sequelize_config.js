a444bcc73657027d651a18b072b99e237fcb9a67
Péter Erdősi <peter.erdosi@extremalelement.com>
2021-11-19 10:31:36 +0100

    changed db production configs

--- db/config/sequelize_config.js
+++ db/config/sequelize_config.js
@@ -38,12 +38,12 @@ module.exports = {
     }
   },
   production: {
-    username: process.env.DB_DEPLOY_USER,
-    password: process.env.DB_DEPLOY_PASS,
-    database: process.env.DB_DEPLOY_DATABASE,
-    host: process.env.DB_DEPLOY_HOST,
-    port: process.env.DB_DEPLOY_PORT,
-    dialect: process.env.DB_DEPLOY_DIALECT,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: process.env.DB_DIALECT,
     dialectOptions: {
       bigNumberStrings: true
     },

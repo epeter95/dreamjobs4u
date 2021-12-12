81520443e37be75b791f3ee1c7f44e049204aa1b
Péter Erdősi <peter.erdosi@extremalelement.com>
2021-12-12 16:11:38 +0100

    removed unused endpoints

--- routes/profiles.js
+++ routes/profiles.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { Profile, User, Role, RoleTranslation } = require('../db/models');
+const { Profile, User } = require('../db/models');
 const JWTManager = require('../middlewares/jwt_manager');
 const FileManager = require('../classes/file_manager');
 
@@ -12,7 +12,7 @@ router.get('/getProfileDataForPublic', async (req, res) => {
     }
     const userData = await User.findOne({
       where: { email: email },
-      include: [Profile, { model: Role, include: RoleTranslation }],
+      include: [Profile],
       attributes: ['email', 'firstName', 'lastName']
     });
     return res.send(userData);
@@ -74,72 +74,6 @@ router.post('/public/editProfilePicture', async (req, res) => {
     console.log(error);
     return res.send({ error: error.name })
   }
-})
-
-
-router.get('/', JWTManager.verifyAdminUser, async (req, res) => {
-  try {
-    const data = await Profile.findAll();
-    return res.send(data);
-  } catch (error) {
-    console.log(error);
-    return res.send({ error: error.name });
-  }
-});
-
-router.get('/:id', JWTManager.verifyAdminUser, async (req, res) => {
-  const paramId = req.params.id;
-  try {
-    let data;
-    if (paramId) {
-      data = await Profile.findOne({
-        where: { id: paramId }
-      });
-    }
-    return res.send(data);
-  } catch (error) {
-    console.log(error);
-    return res.send({ error: error.name });
-  }
-});
-
-router.post('/', JWTManager.verifyAdminUser, async (req, res) => {
-  try {
-    const { userId, phone, profilePicture, cvPath } = req.body;
-    const data = await Profile.create({ userId, phone, profilePicture, cvPath });
-    return res.send({ ok: 'siker' });
-  } catch (error) {
-    console.log(error);
-    return res.send({ error: error.name });
-  }
-});
-
-router.put('/:id', JWTManager.verifyAdminUser, async (req, res) => {
-  const paramId = req.params.id;
-  try {
-    const { userId, phone, profilePicture, cvPath } = req.body;
-    const data = await Profile.update({ userId, phone, profilePicture, cvPath }, {
-      where: { id: paramId },
-    });
-
-    return res.send({ ok: 'siker' });
-  } catch (error) {
-    console.log(error);
-    return res.send({ error: error.name });
-  }
-});
-
-router.delete('/:id', JWTManager.verifyAdminUser, async (req, res) => {
-  const paramId = req.params.id;
-  try {
-    const data = await Profile.destroy({
-      where: { id: paramId }
-    });
-  } catch (error) {
-    console.log(error);
-    return res.send({ error: error.name });
-  }
-  return res.send({ ok: 'siker' });
 });
 
 module.exports = router;
